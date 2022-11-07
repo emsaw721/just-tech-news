2c814d3eeaa09fda216fea9b2d12d969fd799210
Emily Hill <emhillmed2021@gmail.com>
2022-11-07 13:18:05 -0600

    finished 14.1

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const routes = require('./routes');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path'); 
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,8 +13,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars'); 
+
 // turn on routes
 app.use(routes);
+
+// express.static is middleware function that takes all contents of folder and serve as static assets 
+app.use(express.static(path.join(__dirname, 'public'))); 
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
